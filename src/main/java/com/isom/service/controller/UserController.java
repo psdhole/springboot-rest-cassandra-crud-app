ef9960249ee1366bb7f4b11367e1bcfc2d558912
Pushparaj Dhole <pushparaj.dhole23@gmail.com>
2020-05-13 16:13:03 +0200

    Added proper http method annotations

--- src/main/java/com/isom/service/controller/UserController.java
+++ src/main/java/com/isom/service/controller/UserController.java
@@ -5,39 +5,37 @@ import com.isom.service.dao.IUserDao;
 import com.isom.service.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@RestController(value = "/api/v1/users")
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
   @Autowired private IUserDao repository;
 
-  @RequestMapping
+  @GetMapping
   public List<User> getAll() {
     log.info("User list is checking from cassandra");
     return (List<User>) this.repository.findAll();
   }
 
-  @RequestMapping(method = RequestMethod.POST)
+  @PostMapping
   public User create(@RequestBody User user) {
     user.setId(UUIDs.timeBased());
     log.info("User save process has started.");
     return this.repository.save(user);
   }
 
-  @RequestMapping(method = RequestMethod.DELETE)
+  @DeleteMapping
   public void delete(@RequestBody User user) {
     log.info("User delete process has started.");
     this.repository.delete(user);
   }
 
-  @RequestMapping(method = RequestMethod.PUT)
+  @PutMapping
   public User edit(@RequestBody User user) {
     User findUser = this.repository.findById(user.getId());
     if (findUser != null) {
